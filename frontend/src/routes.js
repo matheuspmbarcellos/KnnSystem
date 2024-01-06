85a5568c119d47bd2fbc752fdad1257c16337a4f
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-01-06 01:13:35 -0300

    update routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Fornecedores from './pages/Fornecedores';
+import Pagamento from './pages/Pagamento';
+import ExtratosFinanceiros from './pages/ExtratoFinanceiros';
+import Contratos from './pages/Contratos';
+import Apartamentos from './pages/Apartamentos';
+import Relatorios from './pages/Relatorios';
 
 const MainRoutes = () => {
     return (
@@ -12,6 +19,30 @@ const MainRoutes = () => {
             <Route 
                 path="/Login"
                 element={<Login />}
+            />
+            <Route 
+                path="/Fornecedores"
+                element={<Fornecedores />}
+            />
+            <Route 
+                path="/Pagamento"
+                element={<Pagamento />}
+            />
+            <Route 
+                path="/ExtratosFinanceiros"
+                element={<ExtratosFinanceiros />}
+            />
+            <Route 
+                path="/Contratos"
+                element={<Contratos />}
+            />
+            <Route 
+                path="/Apartamentos"
+                element={<Apartamentos />}
+            />
+            <Route 
+                path="/Relatorios"
+                element={<Relatorios />}
             />
         </Routes>
     )
