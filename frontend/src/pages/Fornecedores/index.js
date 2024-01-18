801d4c8161070222275b631b5b04a2a430af45b9
dudacs <100693199+dudacs@users.noreply.github.com>
2024-01-18 15:38:48 -0300

    Fornecedores

--- frontend/src/pages/Fornecedores/index.js
+++ frontend/src/pages/Fornecedores/index.js
@@ -1,9 +1,20 @@
-import { FornecedoresContainer } from "./styles";
+import { FornecedoresContainer, ButtonContainer } from "./styles";
+import Button from "../../components/Button/Button";
 
 const Fornecedores = () => {
   return (
     <FornecedoresContainer>
-        Fornecedores
+        <h1>FORNECEDORES</h1>
+        <ButtonContainer>
+          <Button secondary as="a" href="./Home">BUSCAR</Button>
+          <Button secondary as="a" href="./Home">CADASTRAR</Button>
+          <Button secondary as="a" href="./Home">ALTERAR</Button>
+          <Button secondary as="a" href="./Home">INATIVAR</Button>
+        </ButtonContainer>
+        <input type='text' placeholder=' CNPJ'/>
+        <input type='text' placeholder=' Código'/>
+        <input type='text' placeholder=' Razão Social'/>
+        <input type='text' placeholder=' Número do Contrato'/>
     </FornecedoresContainer>
   )
 }
--- frontend/src/pages/Fornecedores/styles.js
+++ frontend/src/pages/Fornecedores/styles.js
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
 export const FornecedoresContainer = styled.div`
-    
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+input {
+    color: #131129;
+    background-color: #131129;
+    border-radius: 5px;    
+    height: 5%;
+    width: 30%;
+    padding: 6px;
+    border: solid 0.6px;
+    margin: 5px;
+
+    &:focus {
+        color: white;
+    }
+}
+
+h1 {
+    font-size: 35px;
+    color: #131129;
+    margin: 50px ;
+}
+`
+export const ButtonContainer = styled.div`
+margin: 3% auto auto auto;
 `
