2eebe31d1b6a68c35c29c353baaa416cfb805e7c
dudacs <100693199+dudacs@users.noreply.github.com>
2024-01-19 16:06:54 -0300

    Contratos

--- frontend/src/pages/Contratos/index.js
+++ frontend/src/pages/Contratos/index.js
@@ -1,9 +1,18 @@
-import { ContratosContainer } from "./styles";
+import { ContratosContainer, InputContainer } from "./styles";
+import Button from "../../components/Button/Button";
+import MenuCRUD from "../../components/MenuCRUD";
 
 const Contratos = () => {
   return (
     <ContratosContainer>
-        Contratos
+        <h1>CONTRATOS</h1>
+        <MenuCRUD/>
+        <InputContainer> 
+        <input type='text' placeholder=' Número de Controle'/> 
+        <input type='text' placeholder=' Razão Social do Fornecedor'/>
+        <input type='text' placeholder=' CPNJ do Fornecedor'/>
+        </InputContainer> 
+        <Button>Exibir</Button>
     </ContratosContainer>
   )
 }
