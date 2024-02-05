14c0735b99ae1fc3f0eeca93d3e9b4eeefd52507
dudacs <100693199+dudacs@users.noreply.github.com>
2024-02-05 16:33:52 -0300

    EXTRATO FINANCEIROS - busca

--- frontend/src/pages/ExtratoFinanceiros/index.js
+++ frontend/src/pages/ExtratoFinanceiros/index.js
@@ -1,10 +1,28 @@
-import { ExtratosContainer } from "./styles";
+import { ExtratosContainer, FormContainer, RowContainer  } from "./styles";
+
 import Button from "../../components/Button/Button";
 
 const ExtratosFinanceiros = () => {
   return (
     <ExtratosContainer>
-      <Button>Buscar</Button>
+      <h1>Extratos Financeiros</h1>
+      
+      <form>
+        <RowContainer>
+          <FormContainer>
+            <label>Mês/Ano de início</label>
+            <input type="month" name="inicio" />
+          </FormContainer>
+
+         <FormContainer>
+           <label>Mês/Ano de fim:</label>
+           <input type="month" name="fim" />
+          </FormContainer>
+
+         <Button>Buscar</Button>
+        </RowContainer>
+      </form>
+
     </ExtratosContainer>
   )
 }
--- frontend/src/pages/ExtratoFinanceiros/styles.js
+++ frontend/src/pages/ExtratoFinanceiros/styles.js
@@ -1,5 +1,43 @@
 import styled from "styled-components";
 
 export const ExtratosContainer = styled.div`
-    
+    display: flex;
+    flex-direction: column;
+    color: #131129;
+    width: 100%;
+    align-items: center;
+    padding-top: 3%;
+
+    h1{
+        font-stretch: wide;
+        padding-bottom: 1%;
+    }
+`
+
+export const FormContainer = styled.form`
+    font-size: 10px;
+    display: flex;
+    flex-direction: column;
+    margin-right: 5%;
+    align-items: center;
+    font-stretch: condensed;
+    font-size: 12px;
+    font-weight: 600;
+    margin-right: 10%;
+
+    input {
+        color:blue;
+        border: none;
+        width: 70%;
+        font-size: 11px;
+    }
+`
+export const RowContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    Button{
+        font-size: 11px;
+        height: fit-content;
+    }
 `
