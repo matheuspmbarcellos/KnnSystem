b75388d817bb6277a8790896ff51d5b23fc74d84
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-02-21 02:08:18 -0300

    update: pagamentos

--- frontend/src/pages/Pagamento/Cadastrar/index.jsx
+++ frontend/src/pages/Pagamento/Cadastrar/index.jsx
@@ -38,7 +38,7 @@ const CadastrarPagamento = () => {
         }    
       });
       console.log('Pagamento cadastrado com sucesso:', response.data);
-      alert('Fornecedor cadastrado com sucesso!')
+      alert('Pagamento cadastrado com sucesso!')
       handleLimparFormulario();
     } catch (error) {
         if (error.response) {
@@ -78,20 +78,20 @@ const CadastrarPagamento = () => {
         <MenuPagamentos />
        <BodyContainer>
         <InputContainer>
-          <input type='text' placeholder='Número do Contrato' value={numeroContrato} onChange={e => setNumeroContrato(e.target.value)}/>
-          <input type='text' placeholder='Número da Fatura' value={numeroFatura} onChange={e => setnumeroFatura(e.target.value)}/>
-          <input type='text' placeholder='Data Cadastro' value={dataCadastro} onChange={e => setdataCadastro(e.target.value)}/>
-          <input type='text' placeholder='Data Pagamento' value={dataPagamento} onChange={e => setdataPagamento(e.target.value)}/>
-          <input type='text' placeholder='Valor' value={valor} onChange={e => setvalor(e.target.value)}/>
-          <input type='text' placeholder='Percentual Juros' value={percentualJuros} onChange={e => setpercentualJuros(e.target.value)}/>
+          <input type='text' placeholder='Número do Contrato' value={numeroContrato} onChange={e => setNumeroContrato(e.target.value)} required />
+          <input type='text' placeholder='Número da Fatura' value={numeroFatura} onChange={e => setnumeroFatura(e.target.value)} required />
+          <input type='text' placeholder='Data Cadastro' value={dataCadastro} onChange={e => setdataCadastro(e.target.value)} required />
+          <input type='text' placeholder='Data Pagamento' value={dataPagamento} onChange={e => setdataPagamento(e.target.value)} required />
+          <input type='text' placeholder='Valor' value={valor} onChange={e => setvalor(e.target.value)} required />
+          <input type='text' placeholder='Percentual Juros' value={percentualJuros} onChange={e => setpercentualJuros(e.target.value)} required />
         </InputContainer> 
         <InputContainer> 
-          <input type='email' placeholder='CNPJ Fornecedor' value={cnpjFornecedor} onChange={e => setcnpjFornecedor(e.target.value)}/>
-          <input type='text' placeholder='Razão Social' value={razaoSocial} onChange={e => setrazaoSocial(e.target.value)}/>
-          <input type='email' placeholder='Agência' value={agencia} onChange={e => setagencia(e.target.value)}/>
-          <input type='text' placeholder='Conta Corrente' value={contaCorrente} onChange={e => setcontaCorrente(e.target.value)}/>
-          <input type='text' placeholder='Banco' value={banco} onChange={e => setbanco(e.target.value)}/>
-          <input type='text' placeholder='PIX' value={pix} onChange={e => setpix(e.target.value)}/>
+          <input type='email' placeholder='CNPJ Fornecedor' value={cnpjFornecedor} onChange={e => setcnpjFornecedor(e.target.value)} required />
+          <input type='text' placeholder='Razão Social' value={razaoSocial} onChange={e => setrazaoSocial(e.target.value)} required />
+          <input type='email' placeholder='Agência' value={agencia} onChange={e => setagencia(e.target.value)} required />
+          <input type='text' placeholder='Conta Corrente' value={contaCorrente} onChange={e => setcontaCorrente(e.target.value)} required />
+          <input type='text' placeholder='Banco' value={banco} onChange={e => setbanco(e.target.value)} required />
+          <input type='text' placeholder='PIX' value={pix} onChange={e => setpix(e.target.value)} required />
         </InputContainer> 
         </BodyContainer>
         <Button onClick={submit}>INCLUIR</Button>
