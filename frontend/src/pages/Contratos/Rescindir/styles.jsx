2e32bb91cf9e2111d19da7590e51e6ea3cd21d71
dudacs <100693199+dudacs@users.noreply.github.com>
2024-02-22 16:27:03 -0300

    Update styles.jsx

--- frontend/src/pages/Contratos/Rescindir/styles.jsx
+++ frontend/src/pages/Contratos/Rescindir/styles.jsx
@@ -3,40 +3,66 @@ import styled from "styled-components";
 export const CadastrarContainer = styled.div`
 display: flex;
 flex-direction: column;
-align-items: center;
-
-}
+margin-top: 2%;
 
 h1 {
     font-size: 35px;
     color: #131129;
-    margin: 25px 10px 10px 10px;
+    margin-left: 5%;
+
 }
 
 
 `
 export const BodyContainer = styled.form`
-width: 50%;
+width: 80%;
 display: flex;
-flex-direction: row;
-align-items: center;
-margin: 2%;
-
+flex-direction: column;
+margin-left: 4%;
+margin-top: 2%;
+margin-bottom: 4%;
 `
 
 export const CheckboxContainer = styled.div`
 display: flex;
 flex-direction: row;
-align-items: center;
-padding: 1%;
-
 font-size: 13px;
 color: #131129;
+align-items: center;
+
+label{
+    padding-right: 2%;
+    padding-left: .5%;
+    }
+
+}
 `
 
-export const CheckboxGroup= styled.div`
-width: 95%;
+export const ResultadoContainer = styled.div`
 display: flex;
-flex-direction: column;
-align-items: left;
+flex-direction: row;
+margin-left: 6%;
+
+h1{
+    font-size: 15px;
+    color: red;
+}
+`
+export const FormContainer = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 2%;
+
+input {
+    width: 50%;
+    border-bottom: 1px solid #131129;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+
+    &:focus {
+        color: red;
+    }
+}
 `
